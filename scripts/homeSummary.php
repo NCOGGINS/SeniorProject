900c08e4e9561659d3330c3c453d2ee8885d050e
Nathan Coggins <25321608+2Lazy4This@users.noreply.github.com>
2019-10-21 22:59:28 -0400

    Removed Old Script References

--- scripts/homeSummary.php
+++ scripts/homeSummary.php
@@ -4,7 +4,6 @@
 <title>List of Renters</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/css.css">
 </head>
 <body>
 
--- scripts/newTax.php
+++ scripts/newTax.php
@@ -4,7 +4,6 @@
         <title>Insert </title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="csscss.css">
     </head>
     <body>
 <?php
